56d4d1556e23fa91309c3c14f5b03e5b4862f212
ZaidAj11 <zaidjarrah2002@gmail.com>
2021-10-16 01:28:05 +0100

    added compare functionality

--- crypto.py
+++ crypto.py
@@ -55,11 +55,11 @@ class crypto(commands.Cog):
   def day_change(self,inp, query):
     for i in range (len(self.coin_list)):
       if query == '$':
-        if self.coin_list[i]['symbol'] == inp or self.coin_list[i]['id']:
+        if self.coin_list[i]['symbol'] == inp or self.coin_list[i]['id'] == inp:
           return self.coin_list[i]['price_change_24h']
 
       if query == '%':
-        if self.coin_list[i]['symbol'] == inp or self.coin_list[i]['id']:
+        if self.coin_list[i]['symbol'] == inp or self.coin_list[i]['id'] == inp:
           return self.coin_list[i]['price_change_percentage_24h']
 
     return ('Could not find that one')
@@ -73,6 +73,26 @@ class crypto(commands.Cog):
     out = "Use '$' Prefix before any command \n \n Commands: \n \n'price' returns current price \n \n'mc' returns current market cap \n \n'24$' and '24%' returns the 24 hour change respectively \n \nUse 'dev' to currently see whats currently in development \n \nCredits: ZaidAj11"
     await ctx.send("```\n" + out+ "\n```")
 
+  @commands.command()
+  async def compare(self, ctx, coin1, coin2):
+    for i in range (len(self.coin_list)):
+      
+      if self.coin_list[i]['symbol'] == coin1 or self.coin_list[i]['id'] == coin1:
+        coin1_val = self.coin_list[i]['price_change_percentage_24h']
+      if self.coin_list[i]['symbol'] == coin2 or self.coin_list[i]['id'] == coin2:
+        coin2_val = self.coin_list[i]['price_change_percentage_24h']
+    if(coin1_val<coin2_val):
+      temp = coin1
+      coin1 = coin2
+      coin2 = temp
+      temp = coin1_val
+      coin1_val = coin2_val
+      coin2_val = temp
+    differnce = ((coin1_val - coin2_val)/ coin2_val)
+    await ctx.send(f'{coin1} outperformed {coin2} by %{differnce}')
+
+
+ 
 
 def setup(client):
   client.add_cog(crypto(client))
